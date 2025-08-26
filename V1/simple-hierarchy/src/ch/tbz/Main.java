cfa0a84a1d411afa1826c4c8656088d14058aa73
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2025-08-26 10:39:42 +0200

    finish V1

--- V1/simple-hierarchy/src/ch/tbz/Main.java
+++ V1/simple-hierarchy/src/ch/tbz/Main.java
@@ -7,5 +7,14 @@ public class Main {
         Animal dog = new Dog("Bella", 3);
         Animal cat = new Cat("Kedi", 10);
         Animal bird = new Bird("Zwitacheralla", 5);
+
+        dog.showInfo();
+        cat.showInfo();
+        bird.showInfo();
+
+        dog.makeSound();
+        cat.makeSound();
+        bird.makeSound();
+
     }
 }
