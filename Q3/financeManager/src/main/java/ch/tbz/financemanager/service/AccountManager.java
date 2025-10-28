bfc58a881c62dda62ffc9c9bc6b481ba2fe48672
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2025-10-28 11:38:02 +0100

    add an account manager

---
+++ Q3/financeManager/src/main/java/ch/tbz/financemanager/service/AccountManager.java
@@ -0,0 +1,28 @@
+package ch.tbz.financemanager.service;
+
+import ch.tbz.financemanager.model.Account;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Singleton class to manage all accounts in the system.
+ */
+public class AccountManager {
+    private static AccountManager instance;
+    private List<Account> accounts = new ArrayList<>();
+
+    private AccountManager() {}
+
+    public static AccountManager getInstance() {
+        if (instance == null) instance = new AccountManager();
+        return instance;
+    }
+
+    public void addAccount(Account a) {
+        accounts.add(a);
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+}
