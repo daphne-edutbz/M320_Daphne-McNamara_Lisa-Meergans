854fd982184396fead9f1d7cee7967f9c48c0d17
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2025-10-28 11:36:36 +0100

    add services for Service, Report and Transaction

--- Q3/financeManager/src/main/java/ch/tbz/financemanager/service/ReportService.java
+++ Q3/financeManager/src/main/java/ch/tbz/financemanager/service/ReportService.java
@@ -1,4 +1,17 @@
 package ch.tbz.financemanager.service;
 
+import ch.tbz.financemanager.model.Transaction;
+import ch.tbz.financemanager.report.ReportGenerator;
+import ch.tbz.financemanager.report.ReportStrategy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReportService {
+
+    public String generateReport(List<Transaction> transactions, ReportStrategy strategy) {
+        ReportGenerator generator = new ReportGenerator(strategy, transactions);
+        return generator.generateReport();
+    }
 }
