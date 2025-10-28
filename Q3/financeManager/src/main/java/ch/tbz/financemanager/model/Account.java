610ed9ce1ca705062d9ab7ec0cdfcabd28fa15c2
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2025-10-28 11:39:10 +0100

    add exceptions to account

--- Q3/financeManager/src/main/java/ch/tbz/financemanager/model/Account.java
+++ Q3/financeManager/src/main/java/ch/tbz/financemanager/model/Account.java
@@ -2,6 +2,7 @@ package ch.tbz.financemanager.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import ch.tbz.financemanager.exception.*;
 
 public class Account {
 
@@ -16,10 +17,27 @@ public class Account {
         this.transactions = new ArrayList<>();
     }
 
+    /**
+     * Adds a transaction to this account.
+     * @param t Transaction to add
+     * @throws InvalidTransactionException if amount is 0 or description is missing
+     * @throws BudgetExceededException if adding transaction exceeds budget
+     */
+
     public void addTransaction(Transaction t) {
+        if (t.getAmount() == 0) {
+            throw new InvalidTransactionException("Transaction amount cannot be zero");
+        }
+        if (t.getDescription() == null || t.getDescription().isEmpty()) {
+            throw new InvalidTransactionException("Transaction must have a description");
+        }
+        if (budget != null && budget.isExceededAfter(t, transactions)) {
+            throw new BudgetExceededException("Adding this transaction exceeds your budget!");
+        }
         transactions.add(t);
         balance += t.getAmount();
     }
+
 
     public double getBalance() {
         return balance;
