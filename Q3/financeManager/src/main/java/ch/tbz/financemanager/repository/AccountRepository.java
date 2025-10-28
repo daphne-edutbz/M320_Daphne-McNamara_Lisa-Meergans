7ba337db8c58177e114b9e54c82592934605bf2c
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2025-10-28 11:37:19 +0100

    add the repositories

--- Q3/financeManager/src/main/java/ch/tbz/financemanager/repository/AccountRepository.java
+++ Q3/financeManager/src/main/java/ch/tbz/financemanager/repository/AccountRepository.java
@@ -1,4 +1,9 @@
 package ch.tbz.financemanager.repository;
 
-public class AccountRepository {
+import ch.tbz.financemanager.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
--- Q3/financeManager/src/main/java/ch/tbz/financemanager/repository/TransactionRepository.java
+++ Q3/financeManager/src/main/java/ch/tbz/financemanager/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package ch.tbz.financemanager.repository;
 
-public class TransactionRepository {
+import ch.tbz.financemanager.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
