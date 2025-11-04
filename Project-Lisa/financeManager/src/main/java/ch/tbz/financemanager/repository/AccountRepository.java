19ba029796b1a395bf436acaf010e2c3dbcd8f78
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2025-11-04 09:15:50 +0100

    update uml

--- Project-Lisa/financeManager/src/main/java/ch/tbz/financemanager/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.tbz.financemanager.repository;
-
-import ch.tbz.financemanager.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- Project-Lisa/financeManager/src/main/java/ch/tbz/financemanager/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.tbz.financemanager.repository;
-
-import ch.tbz.financemanager.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
